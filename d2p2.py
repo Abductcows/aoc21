c93a99d5f9e845fdb2b18f0ed442b955496af483
George B <abductcows@gmail.com>
2024-11-30 00:36:35 +0300

    d2

--- d2p2.py
+++ d2p2.py
@@ -10,21 +10,30 @@ def get_lines(filename):
     return lines
 
 
-def get_direction_coords(direction, length):
+def get_direction_coords(direction, length, aim):
     direction_map = {'forward': (1, 0), 'up': (0, 1), 'down': (0, -1)}
-    x, y = direction_map[direction]
-    return x * int(length), y * int(length)
+    length = int(length)
+    x_move, y_move = 0, 0
+
+    if direction == 'forward':
+        y_move = aim * length
+        x_move = length
+    else:
+        aim += direction_map[direction][1] * length
+
+    return x_move, y_move, aim
 
 
 def run(filename):
     lines = get_lines(filename)
 
-    x, y = 0, 0
+    x, y, aim = 0, 0, 0
 
     for line in lines:
-        move = get_direction_coords(*line.split(' '))
+        move = get_direction_coords(*line.split(' '), aim)
         x += move[0]
         y += move[1]
+        aim = move[2]
 
     print(x * y)
 

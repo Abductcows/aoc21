3b808ef061f0e90645d45a9da926582615aedb81
George B <abductcows@gmail.com>
2024-12-05 21:03:06 +0300

    d6

---
+++ d6p2.py
@@ -0,0 +1,21 @@
+from utils import run_with_file, get_input_for_day
+from collections import defaultdict
+
+def run(lines):
+    values = map(int, lines[0].split(','))
+    fish = defaultdict(lambda: 0)
+    for val in values:
+        fish[val] += 1
+
+    for _ in range(256):
+        zero = fish[0]
+        for i in range(1, 9):
+            fish[i - 1] = fish[i]
+        fish[6] += zero
+        fish[8] = zero
+
+    return sum(fish.values())
+
+if __name__ == '__main__':
+    print(run_with_file('example.txt', run))
+    print(run(get_input_for_day('d6')))

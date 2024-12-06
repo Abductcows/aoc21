c5304dbe6f87435cea806f8cd247225c4cdf1ddc
George B <abductcows@gmail.com>
2024-12-06 03:23:21 +0300

    d7p2 definitive solution ;))

--- d7p2.py
+++ d7p2.py
@@ -2,16 +2,10 @@ from utils import get_lines, get_input_for_day
 
 
 def run(lines):
-    nums = sorted(map(int, lines[0].split(',')))
-    # something close to mean?
-    mean, median = sum(nums) / len(nums), nums[len(nums) // 2]
-    step = 1 if mean <= median else -1
-
-    result = 9_223_372_036_854_775_806
-    for point in range(int(mean) - step, int(mean) + 10 * step, step):
-        result = min(result, sum((abs(val - point) * (abs(val - point) + 1) // 2 for val in nums)))
-
-    return result
+    nums = list(map(int, lines[0].split(',')))
+    # black magic?
+    point = round(sum(nums) / len(nums) - 1/2)
+    return sum((abs(val - point) * (abs(val - point) + 1) // 2 for val in nums))
 
 
 if __name__ == '__main__':
